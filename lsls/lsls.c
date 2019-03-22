926584389d630c2fa14eb3267e4796999fc3400a
Daniela Parra <dyparra47@gmail.com>
2019-03-22 11:16:47 -0400

    Add parse command line logic and variables

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,7 +6,25 @@
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
+  // Create variables for opening and reading directory.
+  char *dirname;
+  DIR *dp
+  struct dirent *d
+  struct stat fileinfo;
+
+  // Parse command line.
+  // If user didn't input a directory, use ".".
+  if (argc == 0) {
+    dirname = ".";
+  // Otherwise try opening directory that use specified.
+  } else if (argc == 1) {
+    // Grab second argument that user inputted.
+    dirname = argv[1];
+  // If command line arguments not in the right format, present error to user.
+  } else {
+    dirname = NULL;
+    printf("Error. Not the right format. Try: './lsls DIRECTORY_NAME' or './lsls'");
+  }
 
   // Open directory
 
